ba4d34657decf8d4de6ed3b899d689b51630e8e8
Bala Dasoji <bala.dasoji@gmail.com>
2018-09-06 22:22:58 +0200

    Changed the message for internal user and removed message for non maersk user

--- internal.js
+++ internal.js
@@ -2,10 +2,7 @@ function detectMaersk(username)
 {
     if (username.includes("maersk"))
     {
-	alert("You are Maersk and will be redirected to Azure");
+	alert("You appear to be Maersk internal user and will be redirected to Azure for login");
 	window.location.href = "https://login.microsoftonline.com/maersk.onmicrosoft.com/oauth2/authorize?client_id=91e0c85c-f07a-4206-b514-0cef846596c7&response_type=id_token&redirect_uri=https%3A%2F%2Fmlinternalspa.azurewebsites.net%2Fstore.html&response_mode=fragment&scope=openid&state=12345&nonce=678910";
     }
-    else
-	alert("You are not Maersk");
-
 }
